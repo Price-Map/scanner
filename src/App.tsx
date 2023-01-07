99a5624147694108a8d6c9fcf7de8c2ac3c0039a
Thiago Moura <thiago.moura@sensedia.com>
2023-01-07 20:20:53 -0300

    Add initial integration with netlify function

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react';
+import { Html5QrcodeResult, QrcodeSuccessCallback } from 'html5-qrcode/esm/core';
 import './App.css';
 import Html5QrcodePlugin from './QRCodePlugin';
-import ResultContainerPlugin from './ResultContainerPlugin';
+
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
 
 const App = (props: any) => {
-    const [decodedResults, setDecodedResults] = useState([]);
-    const onNewScanResult = (decodedText: any, decodedResult: never) => {
+    //const [decodedResults, setDecodedResults] = useState([]);
+    const onNewScanResult: QrcodeSuccessCallback = (decodedText: string, decodedResult: Html5QrcodeResult) => {
         console.log("App [result]", decodedResult);
-        setDecodedResults(prev => [...prev, decodedResult]);
+        postData('https://pricemap.netlify.app/.netlify/push', { data: decodedResult })
+            .then((data) => {
+                console.log(data); // JSON data parsed by `data.json()` call
+            });
     };
 
     return (
         <div className="App">
             <section className="App-section">
-                <div className="App-section-title"> Html5-qrcode React demo</div>
-                <br />
-                <br />
-                <br />
                 <Html5QrcodePlugin
                     fps={10}
-                    qrbox={250}
+                    qrbox={300}
                     disableFlip={false}
                     qrCodeSuccessCallback={onNewScanResult}
                 />
-                <ResultContainerPlugin results={decodedResults} />
             </section>
         </div>
     );
--- src/function/push.ts
+++ src/function/push.ts
@@ -1,10 +1,44 @@
-import { Handler, HandlerEvent, HandlerContext, HandlerResponse } from "@netlify/functions";
 
+import { Handler, HandlerEvent, HandlerContext, HandlerResponse } from "@netlify/functions";
+import { DynamoDBClient, PutItemCommand, PutItemCommandInput, PutItemCommandOutput } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+
+const client = new DynamoDBClient({
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID ?? '',
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY ?? ''
+    }
+});
+
+const params: PutItemCommandInput = {
+    TableName: "nfce",
+    Item: marshall({
+        "code": "blah",
+        "year":  2021,
+        "title": "The Big New Movie",
+        "info": {
+            "plot": "Nothing happens at all.",
+            "rating": 0
+        }
+    })
+};
+
+const run = async function (): Promise<PutItemCommandOutput> {
+    try {
+        return await client.send(new PutItemCommand(params));
+    } catch(err: any) {
+        console.error(err)
+        throw new Error(err);
+    }
+};
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext): Promise<HandlerResponse> => {
-
+    const result = await run();
+    console.log(result)
     return {
-        statusCode: 200
+        statusCode: 200,
+        body: JSON.stringify(result)
     }
 };
 
