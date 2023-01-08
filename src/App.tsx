4521efbc18f3cd02d74e29fa018feb0f51ce9d5c
Thiago Moura <thiago.moura@sensedia.com>
2023-01-07 21:13:29 -0300

    Send data to dynamodb

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,24 @@ import './App.css';
 import Html5QrcodePlugin from './QRCodePlugin';
 
 async function postData(url = '', data = {}) {
-    // Default options are marked with *
     const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
+        method: 'POST',
+        mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
+        body: JSON.stringify(data)
     });
-    return response.json(); // parses JSON response into native JavaScript objects
+
+    return response.json();
 }
 
-
-
 const App = (props: any) => {
-    //const [decodedResults, setDecodedResults] = useState([]);
     const onNewScanResult: QrcodeSuccessCallback = (decodedText: string, decodedResult: Html5QrcodeResult) => {
         console.log("App [result]", decodedResult);
-        postData('https://pricemap.netlify.app/.netlify/push', { data: decodedResult })
+        postData('https://pricemap.netlify.app/.netlify/functions/push', { data: decodedText })
             .then((data) => {
-                console.log(data); // JSON data parsed by `data.json()` call
+                console.log(data);
             });
     };
 
--- src/function/push.ts
+++ src/function/push.ts
@@ -3,6 +3,8 @@ import { Handler, HandlerEvent, HandlerContext, HandlerResponse } from "@netlify
 import { DynamoDBClient, PutItemCommand, PutItemCommandInput, PutItemCommandOutput } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
 
+const regexp = /\d{44}/;
+
 const client = new DynamoDBClient({
     region: "us-east-1",
     credentials: {
@@ -11,35 +13,34 @@ const client = new DynamoDBClient({
     }
 });
 
-const params: PutItemCommandInput = {
-    TableName: "nfce",
-    Item: marshall({
-        "code": "blah",
-        "year":  2021,
-        "title": "The Big New Movie",
-        "info": {
-            "plot": "Nothing happens at all.",
-            "rating": 0
-        }
-    })
-};
-
-const run = async function (): Promise<PutItemCommandOutput> {
+const run = async function (url: string) {
     try {
-        return await client.send(new PutItemCommand(params));
-    } catch(err: any) {
-        console.error(err)
-        throw new Error(err);
-    }
-};
+        const code = url.match(regexp)?.[0] ?? '';
+        const item: PutItemCommandInput = {
+            TableName: "nfce",
+            Item: marshall({
+                "code": code,
+                "url": url,
+                "timestamp": Date.now()
+            })
+        }
+        const result = await client.send(new PutItemCommand(item));
+        
+        console.log(result);
+        } catch (err: any) {
+            console.error(err)
+            throw new Error(err);
+        }
+    };
 
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext): Promise<HandlerResponse> => {
-    const result = await run();
-    console.log(result)
-    return {
-        statusCode: 200,
-        body: JSON.stringify(result)
-    }
-};
+    const handler: Handler = async (event: HandlerEvent, context: HandlerContext): Promise<HandlerResponse> => {
+        const url: string = (event.body as any).data
+        await run(url);
+        
+        return {
+            statusCode: 200,
+            //body: JSON.stringify(result)
+        }
+    };
 
-export { handler };
+    export { handler };
