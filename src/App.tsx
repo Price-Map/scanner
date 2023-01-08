a95f4a699a72f8833381b84ebe708b2c347798cc
Thiago Moura <thiago.moura@sensedia.com>
2023-01-07 23:13:30 -0300

    Handle cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
-import { Html5QrcodeResult, QrcodeSuccessCallback } from 'html5-qrcode/esm/core';
 import './App.css';
 import Html5QrcodePlugin from './QRCodePlugin';
 
-const PRICE_MAP_URL = 'https://pricemap.netlify.app/.netlify/functions/push';
-const CODE_REGEXP = /\d{44}/;
-
-export interface Data {
-    code: string
-    url: string
-};
-
-async function postData(url: string, data: Data) {
-    await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-}
-
 const App = (props: any) => {
-    const onNewScanResult: QrcodeSuccessCallback = async (decodedText: string, decodedResult: Html5QrcodeResult) => {
-        const data = {
-            url: decodedText,
-            code: decodedText.match(CODE_REGEXP)?.[0] ?? ''
-        };
-
-        console.log("App data", data);
-
-        await postData(PRICE_MAP_URL, data)
-    };
-
     return (
         <div className="App">
             <section className="App-section">
@@ -40,7 +9,6 @@ const App = (props: any) => {
                     fps={20}
                     qrbox={250}
                     disableFlip={false}
-                    qrCodeSuccessCallback={onNewScanResult}
                 />
             </section>
         </div>
--- src/QRCodePlugin.tsx
+++ src/QRCodePlugin.tsx
@@ -1,28 +1,67 @@
 import { Html5QrcodeScanner } from 'html5-qrcode';
-import { useEffect } from 'react';
+import { Html5QrcodeResult, QrcodeSuccessCallback } from 'html5-qrcode/esm/core';
+import { useEffect, useState } from 'react';
 
 const qrcodeRegionId = "html5qr-code-full-region";
 
-const Html5QrcodePlugin = (props: any) => {
+const PRICE_MAP_URL = 'https://pricemap.netlify.app/.netlify/functions/push';
+const CODE_REGEXP = /\d{44}/;
 
-    useEffect(() => {
-        const config: any = {
-            fps: props.fps,
-            qrbox: props.qrbox,
-            disableFlip: props.disableFlip,
-            //supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+export interface Data {
+    code: string
+    url: string
+};
+
+async function postData(url: string, data: Data) {
+    await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+}
+
+const Html5QrcodePlugin = (props: any) => {
+    const config: any = {
+        fps: props.fps,
+        qrbox: props.qrbox,
+        disableFlip: props.disableFlip,
+        //supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+    };
+    const scan: Html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, false);
+    const [scanner, setScanner] = useState(scan);
+
+    const onNewScanResult: QrcodeSuccessCallback = async (decodedText: string, decodedResult: Html5QrcodeResult) => {
+        const data = {
+            url: decodedText,
+            code: decodedText.match(CODE_REGEXP)?.[0] ?? ''
         };
 
-        const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, false);
-        html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
+        console.log("App data", data);
 
+        await postData(PRICE_MAP_URL, data);
+
+        if (scanner) {
+            await scanner.clear();
+        }
+    };
+
+    if (scanner) {
+        scanner.render(onNewScanResult, ()=>{});
+    }
+
+    useEffect(() => {
+        setScanner(scanner)
+        
         // cleanup function when component will unmount
         return () => {
-            html5QrcodeScanner.clear().catch(error => {
+            scanner.clear().catch(error => {
                 console.error("Failed to clear html5QrcodeScanner. ", error);
             });
         };
-    });
+    }, [scanner]);
 
     return (
         <div id={qrcodeRegionId} />
